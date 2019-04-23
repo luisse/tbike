de549bdd95006dd74ccfd01691559e3d4be7ff2b
root <root@ip-172-31-7-159.ec2.internal>
2019-04-23 15:04:52 -0300

    .

--- app/Config/core.php
+++ app/Config/core.php
@@ -33,7 +33,7 @@ require_once dirname(__DIR__) . '/Vendor/vendor/autoload.php';
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	Configure::write('debug', 0);
 
 /***
 $ENGINE = 'Memcached';
